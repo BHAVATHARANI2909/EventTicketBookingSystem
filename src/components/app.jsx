6fb50405249cb3a50e83fa7ad974432cb2536bdf
ANUCIYA123 <anuciya2003@gmail.com>
2025-11-15 17:23:11 +0530

    modify mge

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import LoginPage from "../Pages/login";
 import RegisterPage from "../Pages/RegisterPage";
 import HomePage from "../dashboard/home";
@@ -9,22 +9,20 @@ import AdminDashboard from "./AdminDashboard";
 import AdminLoginPage from "./Adminlogin";
 import RequestBloodDashboard from "./RequestBlood";
 
-
-
 function App() {
 
     return (
         <>
-            <BrowserRouter>
+            <HashRouter>
                 <Routes>
-                    <Route path="/"element={<LoginPage/>}/>
-                    <Route path="/register" element={<RegisterPage/>}/>
-                    <Route path="/dashboard" element={<HomePage/>}/>
-                    <Route path="/admin" element={<AdminLoginPage/>}/>
-                    <Route path="/admin-dashboard" element={<AdminDashboard/>}/>
-                    <Route path="/request-blood" element={<RequestBloodDashboard/>}/>
-                </Routes> 
-            </BrowserRouter>
+                    <Route path="/" element={<LoginPage />} />
+                    <Route path="/register" element={<RegisterPage />} />
+                    <Route path="/dashboard" element={<HomePage />} />
+                    <Route path="/admin" element={<AdminLoginPage />} />
+                    <Route path="/admin-dashboard" element={<AdminDashboard />} />
+                    <Route path="/request-blood" element={<RequestBloodDashboard />} />
+                </Routes>
+            </HashRouter>
         </>
     );
 }
